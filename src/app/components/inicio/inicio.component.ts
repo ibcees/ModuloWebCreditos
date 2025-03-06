153dfc50ca78e7998253eb618679d28ab9bd2b4d
Ramon Rivera <jesus.espinoza@becasycredito.gob.mx>
2025-03-06 12:00:24 -0700

    Se realizaron cambios en el inicio

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms'; // Importa FormsModule
+
 
 @Component({
   selector: 'app-inicio',
-  imports: [],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, FormsModule],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
 export class InicioComponent {
+  username: string = '';
+  password: string = '';
 
+  onSubmit() {
+    //TODO:: Implementar la lógica para manejar el inicio de sesión
+    console.log('Usuario:', this.username);
+    console.log('Contraseña:', this.password);
+  }
 }
